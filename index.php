917277460239f79d58e0010e42ddc4183719eb6e
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-11-15 01:31:40 +0800

    dashboard page

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Dashboard.css">
+    <link rel="stylesheet" href="css/Navbar.css">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo-cont"></div>
+        <ul class="nav-list">
+            <li class="nav-item active">Dashboard</li>
+            <li class="nav-item"><a href="./register.php" class="nav-link">Register</a></li>
+            <li class="nav-item"><a href="./reports.php" class="nav-link">Reports</a></li>
+        </ul>
+        <div class="accounts-cont"></div>
+    </nav>
+</body>
+
+</html>
