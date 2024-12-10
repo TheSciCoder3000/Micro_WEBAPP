bfd06b777c788bf0ad98ff428e15c16a6fe4d49c
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-12-11 01:43:27 +0800

    dual upload file update

--- upload_util.php
+++ upload_util.php
@@ -3,45 +3,61 @@
 print_r($_FILES);
 
 $finfo = new finfo(FILEINFO_MIME_TYPE);
-$mime_type = $finfo->file($_FILES['file']['tmp_name']);
+$mime_type_1 = $finfo->file($_FILES['file-1']['tmp_name']);
+$mime_type_full = $finfo->file($_FILES['file-full']['tmp_name']);
 
 $mime_types = ["application/pdf"];
 
-if (!in_array($_FILES['file']['type'], $mime_types)) {
+if (!in_array($_FILES['file-1']['type'], $mime_types) && !in_array($_FILES['file-full']['type'], $mime_types)) {
     exit("File type not supported");
 }
 
-$filename = $_FILES['file']['name'];
-$destination = __DIR__ . "\uploads/" . $filename;
-$filesize = $_FILES['file']['size'];
 
-$upload_success = move_uploaded_file($_FILES['file']['tmp_name'], $destination);
+$filename_1 = $_FILES['file-1']['name'];
+$filesize_1 = $_FILES['file-1']['size'];
+$destination_1 = __DIR__ . '\uploads/' . $filename_1;
 
-if ($_FILES['file']['error'] == 0) {
-    echo "Upload success";
+$filename_full = $_FILES['file-full']['name'];
+$filesize_full = $_FILES['file-full']['size'];
+$destination_full = __DIR__ . '\uploads/' . $filename_full;
 
-    $serverName = "NeurosLaptop\SQLEXPRESS";
+$upload_success_1 = move_uploaded_file($_FILES['file-1']['tmp_name'], $destination_1);
+$upload_success_full = move_uploaded_file($_FILES['file-full']['tmp_name'], $destination_full);
 
-    $connectionOptions = [
-        "Database" => "WEBAPP",
-        "Uid" => "",
-        "PWD" => ""
-    ];
 
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-    $sql_title = "SELECT * FROM TITLE WHERE TITLE_ID = (SELECT IDENT_CURRENT('TITLE'));";
-    $title_res = sqlsrv_query($conn, $sql_title);
-    $title_id = sqlsrv_fetch_array($title_res)['TITLE_ID'];
+$serverName = "NeurosLaptop\SQLEXPRESS";
 
-    $sql = "INSERT INTO PDF_UPLOAD (PDF_NAME, PDF_SIZE, PDF_PATH, TITLE_ID) 
-            VALUES ('$filename', '$filesize', '$destination', '$title_id');";
-    $sql_res = sqlsrv_query($conn, $sql);
+$connectionOptions = [
+    "Database" => "WEBAPP",
+    "Uid" => "",
+    "PWD" => ""
+];
 
-    if ($sql_res) {
-        echo "<br>Uploaded successful to the db";
-        header('Location: success.php');
-    } else {
-        echo "<br>error: db upload";
-    }
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+$sql_title = "SELECT * FROM TITLE WHERE TITLE_ID = (SELECT IDENT_CURRENT('TITLE'));";
+$title_res = sqlsrv_query($conn, $sql_title);
+$title_id = sqlsrv_fetch_array($title_res)['TITLE_ID'];
+
+$sql_res_1 = false;
+$sql_res_full = false;
+
+if ($_FILES['file-1']['error'] == 0) {
+    $sql = "INSERT INTO PDF_ABSTRACT (PDF_NAME, PDF_SIZE, PDF_PATH, TITLE_ID) 
+            VALUES ('$filename_1', '$filesize_1', '$destination_1', '$title_id');";
+    $sql_res_1 = sqlsrv_query($conn, $sql);
+}
+
+if ($_FILES['file-full']['error'] == 0) {
+    $sql = "INSERT INTO PDF_FULL (PDF_NAME, PDF_SIZE, PDF_PATH, TITLE_ID) 
+            VALUES ('$filename_full', '$filesize_full', '$destination_full', '$title_id');";
+    $sql_res_full = sqlsrv_query($conn, $sql);
+}
+
+if ($sql_res_1 && $sql_res_full) {
+    echo "<br>Uploaded successful to the db";
+    header('Location: success.php');
+} else {
+    echo "<br>error: db upload";
 }
