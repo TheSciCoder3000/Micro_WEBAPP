37bab4208c25c09bb2f029ddbd4da8a62f47f75f
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-12-13 00:54:28 +0800

    download file fix

--- download_file.php
+++ download_file.php
@@ -1,11 +1,9 @@
 <?php
-$serverName = "NeurosLaptop\SQLEXPRESS";
-$connectionOptions = [
-    "database" => "WEBAPP"
-];
-$conn = sqlsrv_connect($serverName, $connectionOptions);
 
 $file_path = $_POST['file-path'];
 header('Content-type: application/octet-stream');
 header("Content-Transfer-encoding: utf-8");
 header("Content-disposition: attachment; filename=\"" . basename($file_path) . "\"");
+header("Content-length: " . filesize($file_path));
+readfile($file_path);
+flush();
